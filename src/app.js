923deceda3e03b3d2362596b980e3a901f703850
zxu <1583261763@qq.com>
2020-04-03 21:33:31 +0800

    添加babel

--- src/app.js
+++ src/app.js
@@ -1,38 +1,19 @@
 import React from 'react'
-import axios from 'axios'
-import { hot } from 'react-hot-loader/root'
 class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      name: null,
-      isLoaded: false
+      text: 'React Demo'
     }
-  }
-  componentDidMount() {
-    const _this = this
-    axios
-      .get('/api/example')
-      .then(function(response) {
-        _this.setState({
-          status: response.status
-        })
-      })
-      .catch(function(error) {
-        console.log(error)
-        _this.setState({
-          status: 'failed'
-        })
-      })
   }
   render() {
     return (
       <div>
-        <p> load {this.state.status} </p>
+        <p> {this.state.text} </p>
       </div>
     )
   }
 }
 
-export default hot(App)
+export default App
